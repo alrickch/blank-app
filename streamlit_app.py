aabb11b5ca3c24e8d96e255769937162ef3ee713
alrickch <149497087+alrickch@users.noreply.github.com>
2024-11-13 06:49:46 +0000

    updated iframe size to be 1024 x 1024

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ def run():
     iframe_url = "https://cdn.botpress.cloud/webchat/v2.2/shareable.html?configUrl=https://files.bpcontent.cloud/2024/11/13/06/20241113061509-EYEAY5FQ.json"  # Replace with your bot's URL
 
     # Embed the iFrame with the Botpress chatbot
-    components.iframe(iframe_url, height=600, width=400)  # Adjust as needed
+    components.iframe(iframe_url, height=1024, width=1024)  # Adjust as needed
 
 if __name__ == "__main__":
     run()
