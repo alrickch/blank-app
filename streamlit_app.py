6942ac79100f3c82baf7e50505ed03d8099a975b
alrickch <149497087+alrickch@users.noreply.github.com>
2024-11-13 08:05:17 +0000

    revert to iframe as js didn't work

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,15 +2,11 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 def run():
-    # HTML code with Botpress scripts
-    st.title('Botpress Example')
-    botpress_code = """
-    
-        <script src="https://cdn.botpress.cloud/webchat/v2.2/inject.js"></script>
-        <script src="https://files.bpcontent.cloud/2024/11/13/06/20241113061509-EA4PHMES.js"></script>
-    """
-    # Use components.html to render the JavaScript in Streamlit
-    #components.html(botpress_code, height=1024, width=1024)  # Adjust the height and width as needed
-    st.markdown(botpress_code, unsafe_allow_html=True)
+    # Assuming you have a direct URL to embed the Botpress chat
+    iframe_url = "https://cdn.botpress.cloud/webchat/v2.2/shareable.html?configUrl=https://files.bpcontent.cloud/2024/11/13/06/20241113061509-EYEAY5FQ.json"  # Replace with your bot's URL
+
+    # Embed the iFrame with the Botpress chatbot
+    components.iframe(iframe_url, height=1024, width=1024)  # Adjust as needed
+
 if __name__ == "__main__":
     run()
