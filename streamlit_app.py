a6a20c9014e224885ce2b9386dd3306c251b947c
alrickch <149497087+alrickch@users.noreply.github.com>
2024-11-13 07:10:53 +0000

    changed to js embed instead of iframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,13 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 def run():
-    # Assuming you have a direct URL to embed the Botpress chat
-    iframe_url = "https://cdn.botpress.cloud/webchat/v2.2/shareable.html?configUrl=https://files.bpcontent.cloud/2024/11/13/06/20241113061509-EYEAY5FQ.json"  # Replace with your bot's URL
-
-    # Embed the iFrame with the Botpress chatbot
-    components.iframe(iframe_url, height=1024, width=1024)  # Adjust as needed
+    # HTML code with Botpress scripts
+    botpress_code = """
+        <script src="https://cdn.botpress.cloud/webchat/v2.2/inject.js"></script>
+        <script src="https://files.bpcontent.cloud/2024/11/13/06/20241113061509-EA4PHMES.js"></script>
+    """
+    # Use components.html to render the JavaScript in Streamlit
+    components.html(botpress_code, height=800, width=600)  # Adjust the height and width as needed
 
 if __name__ == "__main__":
     run()
