72d49b22ed0a8a5615959996039be3daad847c4f
alrickch <149497087+alrickch@users.noreply.github.com>
2024-11-13 07:43:49 +0000

    changed to st.markdown instead of st.components

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,14 @@ import streamlit.components.v1 as components
 
 def run():
     # HTML code with Botpress scripts
+    st.title('Botpress Example')
     botpress_code = """
+    
         <script src="https://cdn.botpress.cloud/webchat/v2.2/inject.js"></script>
         <script src="https://files.bpcontent.cloud/2024/11/13/06/20241113061509-EA4PHMES.js"></script>
     """
     # Use components.html to render the JavaScript in Streamlit
-    components.html(botpress_code, height=1024, width=1024)  # Adjust the height and width as needed
-
+    #components.html(botpress_code, height=1024, width=1024)  # Adjust the height and width as needed
+    st.markdown(botpress_code, unsafe_allow_html=True)
 if __name__ == "__main__":
     run()
