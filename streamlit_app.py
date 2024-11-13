7befc712323a656a50015383770ba9058cb7ba3c
alrickch <149497087+alrickch@users.noreply.github.com>
2024-11-13 07:12:23 +0000

    changed size to 1024x1024

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ def run():
         <script src="https://files.bpcontent.cloud/2024/11/13/06/20241113061509-EA4PHMES.js"></script>
     """
     # Use components.html to render the JavaScript in Streamlit
-    components.html(botpress_code, height=800, width=600)  # Adjust the height and width as needed
+    components.html(botpress_code, height=1024, width=1024)  # Adjust the height and width as needed
 
 if __name__ == "__main__":
     run()
